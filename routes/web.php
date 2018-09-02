04d68d304bac2966d7ce5498ca629996673513cb
kimjihee104 <kimjihee104@naver.com>
2018-09-02 16:47:26 +0900

    routing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/* php view 파일(welcome.blade.php)  return
+ Route::get('/', function () {
+     return veiw('welcome');
+ });
+*/
+
+/* 문자열 return
+ Route::get('/', function () {
+     return '<h1>Hello Foo</h1>';
+ });
+*/
+
+/* url 파라미터 return
+Route::get('/{foo?}', function ($foo = 'bar') {
+    return $foo;
+});
+*/
+
+/* 1) url 파라미터 return 조건 => 숫자, 영어 소문자 , 영어 대문자로 구성된 세자리 글자
+Route::pattern('foo','[0-9a-zA-Z]{3}');
+Route::get('/{foo?}', function ($foo = 'bar') {
+    return $foo;
+});
+*/
+
+/* 2) url 파라미터 return 조건 => 숫자, 영어 소문자 , 영어 대문자로 구성된 세자리 글자
+Route::get('/{foo?}', function ($foo = 'bar') {
+    return $foo;
+})->where('foo','[0-9a-zA-Z]{3}');
+*/
+
+/* 라우트에 이름 부여 리디렉션 */
+Route::get('/',[
+  'as' => 'home', //라우트 이름
+  function(){
+    return '제 이름은 "home" 입니다.';
+  }
+
+]);
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });
